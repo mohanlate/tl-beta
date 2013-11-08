0620b193a7b2c1f32df0095a0db1d1f70a723334
mohanlate <mohan.late@gmail.com>
2013-11-08 21:39:12 +0530

    use test.html

---
+++ app/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('toyBuddyApp', [
+  'ngRoute',
+  'toyBuddyApp.controllers',
+  'toyBuddyApp.filters',
+  'toyBuddyApp.services',
+  'toyBuddyApp.animations'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'ctrlHome'});
+  $routeProvider.when('/catalog', {templateUrl: 'partials/catalog.html', controller: 'ctrlCatalog'});
+  $routeProvider.when('/catalog/:id', {templateUrl: 'partials/learningResource.html', controller: 'ctrlLearningResource'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
